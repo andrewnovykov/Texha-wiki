baf053de7d6480adb5774b6ad4e9757bec8ccd76
papapin <papapin777@gmail.com>
2015-12-05 16:44:26 +0200

    install sinple form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
+
+	before_action :find_article, only: [:show]
+
 	def index
 		
 	end
+
+	def show
+		
+	end
+
 
 	def new
 		@article = Article.new
@@ -17,6 +25,10 @@ class ArticlesController < ApplicationController
 	end
 
 	private
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
 
 	def article_params
 		params.require(:article).permit(:title, :titleEn, :titleFr, :titleCn, :titleEs, :titleUa,  :content, :contentEn, :contentFr, :contentCn, :contentEs,  :contentUa,)
